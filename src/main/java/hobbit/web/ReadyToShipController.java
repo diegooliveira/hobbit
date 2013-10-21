b6ec04d888b488275d36bb44131867197304bbf8
Diego de Oliveira <Diego Oliveira diego@diegooliveira.com>
2013-10-21 00:04:40 -0200

    Inicio da montagem da tela de orçamento para entrega futura.

---
+++ src/main/java/hobbit/web/ReadyToShipController.java
@@ -0,0 +1,14 @@
+package hobbit.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReadyToShipController {
+    
+    @RequestMapping("/pe")
+    public void get() {
+
+    }
+
+}
