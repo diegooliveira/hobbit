bf9fdcad2f46a634b1fd256e279160883fa8446e
Diego de Oliveira <Diego Oliveira diego@diegooliveira.com>
2013-10-20 22:32:24 -0200

    Primeiro commit, projeto simples, roda, abre uma página e só.

---
+++ src/main/java/hobbit/HobibtServer.java
@@ -0,0 +1,15 @@
+package hobbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class HobibtServer {
+    
+    public static void main(String args[]){
+        SpringApplication.run(HobibtServer.class, args);
+    }
+
+}
